e0ecbd433b400ddc09eab17847c3c8d89986c681
Asen Hodzhev <asenhodzhev@yahoo.com>
2017-06-08 17:37:14 +0300

    Changed connection string to production.

--- EF-HomeDelivery/HD.DbContext/ApplicationDbContext.cs
+++ EF-HomeDelivery/HD.DbContext/ApplicationDbContext.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using HD.DbContext.Migrations;
 using HD.Entities;
 using HD.Entities.Common;
 using HD.Entities.Models;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace HD.DbContext
@@ -23,8 +16,8 @@ namespace HD.DbContext
         public virtual IDbSet<City> Cities { get; set; }
 
         public ApplicationDbContext()
-            : base("HDDevConnection", throwIfV1Schema: false)
-           // : base("HDConnection", throwIfV1Schema: false)
+            //: base("HDDevConnection", throwIfV1Schema: false)
+          : base("HDConnection", throwIfV1Schema: false)
         {
 
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
