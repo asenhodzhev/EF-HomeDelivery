e5b4b748bc20b2e4bb646ea8e48465eb20e08f5c
Asen Hodzhev <asenhodzhev@yahoo.com>
2017-06-08 16:26:20 +0300

    Clean project architecture. Added External DB.

---
+++ EF-HomeDelivery/HD.DbContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using HD.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace HD.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("HDDevConnection", throwIfV1Schema: false)
+           // : base("HDConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
